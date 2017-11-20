3c72a064b8915aa5592bc3a7fe713643d2c0c098
Camila Maya <camilamaya21@gmail.com>
2017-11-20 16:10:55 +0100

    User Model v.1

--- models/User.js
+++ models/User.js
@@ -1,19 +1,20 @@
 const mongoose = require('mongoose')
 const Schema   = mongoose.Schema
+const OBJECTIVES   = require('./running-objetives');
 
 const userSchema = new Schema({
-  username: String,
-  password: String,
-  email: String,
-  role: {
-    type: String,
-    enum: [
-      'admin',
-      'user',
-      'professional'
-    ],
-    default: 'user'
-  }
+  name: String,
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  objetives: { type: String, enum: OBJECTIVES, required: true },
+  events : { type: Schema.Types.ObjectId, ref: 'Event'},
+  aboutMe: String,
+  city: String,
+  comment: { type: Schema.Types.ObjectId, ref: 'Comment'},
+  follow: { type: Number, default: 0 },
+  followers: { type: Number, default: 0 },
+  imgUrl: { type: String, default: "https://placeholdit.imgix.net/~text?txtsize=33&txt=250%C3%97250&w=250&h=250" }
 }, {
   timestamps: {
     createdAt: 'created_at',
---
+++ models/running-objetives.js
@@ -0,0 +1,8 @@
+module.exports = [
+  "empezar a correr",
+  "10K",
+  "media maratón",
+  "maratón",
+  "trail",
+  "correr por diversión",
+];
