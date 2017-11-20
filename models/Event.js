1fc24d648368cbbdc41ae371ea87a11d94c17f9f
Clementina <mataclemy@gmail.com>
2017-11-20 20:01:56 +0100

    fixed schema event

--- models/Event.js
+++ models/Event.js
@@ -5,9 +5,10 @@ const eventSchema = new Schema({
   eventName: { type: String, required: true },
   description: { type: String, required: true },
   date: { type: Date, default: Date.now },
-  lat: String,
-  log: String,
-  imgUrl: { type: String, default: "https://www.ctchamartin.es/wp-content/uploads/2016/04/running-wallpaper-2.jpg" }
+  lat: Number,
+  log: Number,
+  imgUrl: { type: String, default: "https://www.ctchamartin.es/wp-content/uploads/2016/04/running-wallpaper-2.jpg" },
+  website: String,
 }, {
   timestamps: {
     createdAt: 'created_at',
