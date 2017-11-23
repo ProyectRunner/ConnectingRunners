557e8e12ec8c82377b1b4ae870f9ca65061fcf05
Clementina <mataclemy@gmail.com>
2017-11-23 15:35:38 +0100

    new map in event details

---
+++ public/js/map-detail.js
@@ -0,0 +1,17 @@
+
+  function initMap() {
+var eventPosition = {lat: mapevent.lat, lng: mapevent.log};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 18,
+    center: eventPosition
+  });
+
+  var marker = new google.maps.Marker({
+    position: eventPosition,
+    map: map,
+    icon: ("/images/favicon.ico"),
+    title: 'Hello World!'
+  });
+}
+initMap();
