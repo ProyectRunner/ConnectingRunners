4456d2769a57db5833b36a659bfd6ce1c1e900a5
Clementina <mataclemy@gmail.com>
2017-11-24 02:13:28 +0100

    mixed with cami and working

--- config/express.js
+++ config/express.js
@@ -8,7 +8,6 @@ const layouts = require('express-ejs-layouts');
 const config = require('./config.js');
 const mongoose = require('mongoose');
 const path = require('path');
-const ejsLint = require('ejs-lint');
 
 module.exports = function(app) {
 
